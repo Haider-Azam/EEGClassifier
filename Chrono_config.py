f90d49cd9c88d738790e9b39b902459c4099086f
Haider-Azam <m.haiderazam@yahoo.com>
2023-09-22 14:36:15 +0500

    Recreation of ChronoNet
    Recreating ChronoNet in pytorch instead of Tensorflow.

---
+++ Chrono_config.py
@@ -0,0 +1,5 @@
+# Dataset folder path 
+normaldir = "E:/NMT_dataset/normal/train"
+abnormaldir= "E:/NMT_dataset/abnormal/train"
+eval_normaldir="E:/NMT_dataset/normal/eval"
+eval_abnormaldir= "E:/NMT_dataset/abnormal/eval"
